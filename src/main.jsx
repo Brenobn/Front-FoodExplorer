5b252de8a8322144f019566b711cef32ae7fdbdc
Brenobn <brenobragabn@gmail.com>
2024-07-27 13:09:29 -0300

    Importing the theme file from the styles folder into the main.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 
+import theme from './styles/theme';
+
 import { Home } from './pages/Home';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider>
+    <ThemeProvider theme={theme}>
       <Home />
     </ThemeProvider>
   </React.StrictMode>,
