26b6c20f11f523d7a0f8f3cdc393e7506e835b6e
Brenobn <brenobragabn@gmail.com>
2024-07-27 13:31:48 -0300

    Setting :root with font-size of 62.5%.

--- src/styles/global.js
+++ src/styles/global.js
@@ -7,6 +7,10 @@ export default createGlobalStyle`
     box-sizing: border-box;
   }
 
+  :root {
+    font-size: 62,5%;
+  }
+
   body {
     background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
   }
