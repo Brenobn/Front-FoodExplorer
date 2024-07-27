51dbce0883a2bc385874846a7e2af1e0de26c42c
Brenobn <brenobragabn@gmail.com>
2024-07-27 19:42:28 -0300

    Importing Button to Home Page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,11 @@
 import { Container } from "./styles";
 
+import { Button } from "../../components/Button";
+
 export function Home() {
   return(
     <Container>
+      <Button />
       <h1>Hello world!</h1>
     </Container>
   );
