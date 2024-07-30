6d4e50dab883c4b375a1ec891623b12452114444
Brenobn <brenobragabn@gmail.com>
2024-07-30 14:40:04 -0300

    Starting the Header component style

---
+++ src/components/Header/styles.js
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.header`
+  grid-area: header;
+
+  height: 11.4rem;
+  width: 100%;
+
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_HEADER};
+`;
+
+export const HeaderItensContainer = styled.div`
+  display: flex;
+  height: 3.4rem;
+  align-items: center;
+  justify-content: center;
+  gap: 1.6rem;
+  margin: 5.6rem 2.8rem 2.4rem 2.8rem;
+`;
+
+export const LogoText = styled.div`
+  display: flex;
+  height: 3.4rem;
+  align-items: center;
+  gap: 1.6rem;
+
+  > img {
+    width: 2.4rem;
+    height: 2.4rem;
+  }
+
+  > h1 {
+    color: ${({ theme }) => theme.COLORS.SIMPLE_WHITE};
+    font-family: 'Roboto', sans-serif;
+    font-size: 2.0rem;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+  }
+`;
