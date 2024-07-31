64101d08103820bca26cfcc17c7160521436f371
Brenobn <brenobragabn@gmail.com>
2024-07-31 13:16:14 -0300

    Importing Macarons.png, creating Outdoor and styling it using media queries making it responsive

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,16 @@
-import { Container } from "./styles";
+import { Container, ContainerOutdoor } from "./styles";
 
 import { Header } from "../../components/Header";
+
+import Macarrons from "../../assets/Macarrons.png";
 
 export function Home() {
   return(
     <Container>
       <Header />
+      <ContainerOutdoor>
+        <img src={Macarrons} alt="Imagem de macarrons" />
+      </ContainerOutdoor>
     </Container>
   );
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -11,3 +11,45 @@ export const Container = styled.div`
   "header"
   "content";
 `;
+
+export const ContainerOutdoor = styled.div`
+  width: 37.6rem;
+  height: 12.0rem;
+
+  border-radius: 0.3rem;
+  background: linear-gradient(180deg, #091E26 0%, #00131C 100%);
+
+  position: relative;
+
+  > img {
+    width: 19.0rem;
+    height: 15.0rem;
+
+    top: -3.2rem;
+    left: -2.4rem;
+
+    opacity: 0.8;
+    position: absolute;
+
+    @media (min-width: 768px) {
+      width: 40.0rem;
+      height: 28.6rem;
+
+      left: -3.4rem;
+      top: -8.8rem;
+    }
+  }
+
+  @media (max-width: 768px) {
+    margin: 4.6rem 4.4rem 6.2rem 5.0rem;
+  }
+
+  @media (min-width: 768px) {
+    width: 85%;
+    height: 20.0rem;
+
+    border-radius: 0.8rem;
+
+    margin: 6.0rem 10.0rem 6.2rem 5.0rem;
+  }
+`;
