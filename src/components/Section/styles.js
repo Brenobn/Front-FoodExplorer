95dd10e7ae93f5d278cea24a6b027a96c26aac93
Brenobn <brenobragabn@gmail.com>
2024-08-01 12:22:23 -0300

    Using media queries to make h2 responsive

--- src/components/Section/styles.js
+++ src/components/Section/styles.js
@@ -12,4 +12,11 @@ export const SectionContainer = styled.section`
     font-weight: 500;
     line-height: 2.5rem;
   }
+
+  @media (min-width: 768px) {
+    > h2 {
+      font-size: 3.2rem;
+      line-height: 4.5rem;
+    }
+  }
 `;
