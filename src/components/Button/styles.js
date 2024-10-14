d02affe35a3445c8f9240f74499dbe25272cafb7
Brenobn <brenobragabn@gmail.com>
2024-10-14 08:51:49 -0300

    Removing fixed measurements from the button component

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -7,13 +7,12 @@ export const Container = styled.button`
   gap: 0.8rem;
 
   width: 21.6rem;
-  height: 5.6rem;
   background-color: ${({ theme }) => theme.COLORS.RED};
   color: ${({ theme }) => theme.COLORS.RED};
   color: ${({ theme }) => theme.COLORS.SIMPLE_WHITE};
   font-weight: 500;
 
-  padding: 1.2rem 2.4rem;
+  padding: 1.2rem 3.2rem;
   
   border: none;
   border-radius: .5rem;
