daf345f9aa0c93e2940d9aa3326fda7e45e8c8b4
Brenobn <brenobragabn@gmail.com>
2024-10-19 10:25:15 -0300

    Adjusting HTML style and structure to be responsive using media query and clamp

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -7,26 +7,29 @@ import Polygon from "../../assets/Polygon.png";
 export function SignIn() {
   return(
     <Container>
-      <TitleContainer>
-        <img src={Polygon} alt="Logo do app" />
-        <Title>
-          Food explorer
-        </Title>
-      </TitleContainer>
-      <FormContainer action="subimit">
-        <div>
-          <label>
-            Email
-            <input type="text" placeholder="Exemplo: exemplo@exemplo.com.br" />
-          </label>
-          <label>
-            Senha
-            <input type="password" placeholder="No mínimo 6 caracteres" />
-          </label>
-          <Button title="Entrar" />
-          <a href="#">Criar uma conta</a>
-        </div>
-      </FormContainer>
+      <div className="largerScreen">
+        <TitleContainer>
+          <img src={Polygon} alt="Logo do app" />
+          <Title>
+            Food explorer
+          </Title>
+        </TitleContainer>
+        <FormContainer action="subimit">
+          <div>
+            <h2>Faça login</h2>
+            <label>
+              Email
+              <input type="text" placeholder="Exemplo: exemplo@exemplo.com.br" />
+            </label>
+            <label>
+              Senha
+              <input type="password" placeholder="No mínimo 6 caracteres" />
+            </label>
+            <Button title="Entrar" />
+            <a href="#">Criar uma conta</a>
+          </div>
+        </FormContainer>
+      </div>
     </Container>
   );
 }
