3340e0ad61cad3eb3e40c2371a1b47739c448fcc
Brenobn <brenobragabn@gmail.com>
2024-10-19 10:27:04 -0300

    Adjusting HTML style and structure to let SignUp page responsive using media query and clamp

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -7,30 +7,34 @@ import Polygon from "../../assets/Polygon.png";
 export function SignUp() {
   return(
     <Container>
-      <TitleContainer>
-        <img src={Polygon} alt="Logo do app" />
-        <Title>
-          Food explorer
-        </Title>
-      </TitleContainer>
-      <FormContainer action="subimit">
-        <div>
-          <label>
-            Seu nome
-            <input type="name" placeholder="Exemplo: Maria da Silva" />
-          </label>
-          <label>
-            Email
-            <input type="text" placeholder="Exemplo: exemplo@exemplo.com.br" />
-          </label>
-          <label>
-            Senha
-            <input type="password" placeholder="No mínimo 6 caracteres" />
-          </label>
-          <Button title="Entrar" />
-          <a href="#">Já tenho uma conta</a>
-        </div>
-      </FormContainer>
+      <div className="largerScreen">
+
+        <TitleContainer>
+          <img src={Polygon} alt="Logo do app" />
+          <Title>
+            Food explorer
+          </Title>
+        </TitleContainer>
+        <FormContainer action="subimit">
+          <div>
+            <h2>Crie sua conta</h2>
+            <label>
+              Seu nome
+              <input type="name" placeholder="Exemplo: Maria da Silva" />
+            </label>
+            <label>
+              Email
+              <input type="text" placeholder="Exemplo: exemplo@exemplo.com.br" />
+            </label>
+            <label>
+              Senha
+              <input type="password" placeholder="No mínimo 6 caracteres" />
+            </label>
+            <Button title="Criar conta" />
+            <a href="#">Já tenho uma conta</a>
+          </div>
+        </FormContainer>
+      </div>
     </Container>
   );
 }
--- src/pages/SignUp/styles.js
+++ src/pages/SignUp/styles.js
@@ -2,8 +2,8 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   width: 100%;
-  min-width: 42.8rem;
-  min-height: 92.6rem;
+  max-width: 136.8rem;
+  height: 100vh;
 
   display: flex;
   flex-direction: column;
@@ -11,6 +11,25 @@ export const Container = styled.div`
   justify-content: center;
 
   background: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+
+  overflow: hidden;
+
+  padding: 0 2rem;
+
+  margin: 0 auto;
+
+  @media (min-width: 768px) {
+    height: 100vh;
+  }
+
+  .largerScreen {
+    @media (min-width: 768px) {
+      display: flex;
+      flex-direction: row;
+      gap: clamp(1rem, 6vw, 20rem);
+    }
+
+  }
 `;
 
 export const TitleContainer = styled.div`
@@ -20,11 +39,23 @@ export const TitleContainer = styled.div`
   gap: 1.2rem;
   width: 30rem;
 
-  margin: 15.8rem 8.4rem 7.4rem 6.4rem;
+  margin: 32rem 8.4rem 7.4rem 6.4rem;
+  
+  @media (min-width: 768px) {
+    width: 42rem;
+    gap: 1.8rem;
+
+    margin: 20rem 0 30rem 6rem;
+  }
 
   > img {
-    width: 4.4rem;
-    height: 4.4rem;
+    width: 3.8rem;
+    height: 3.8rem;
+
+    @media (min-width: 768px) {
+      width: 4.2rem;
+      height: 4.2rem;
+    }
   }
 `;
 
@@ -34,6 +65,10 @@ export const Title = styled.h1`
   font-style: normal;
   font-weight: 700;
   line-height: normal;
+  
+  @media (min-width: 768px) {
+    font-size: 4.2rem;
+  }  
 `;
 
 export const FormContainer = styled.form`
@@ -44,12 +79,34 @@ export const FormContainer = styled.form`
 
   margin: 0 4.6rem 34rem 6.4rem;
 
+  @media (min-width: 768px) {
+    margin: 4rem 10rem 4rem 0;
+    border-radius: 1.6rem;
+    width: clamp(10rem, 50vw, 47rem);
+    min-height: 62rem;
+  }
+
   div {
     display: flex;
     flex-direction: column;
     align-items: center;
     gap: 3.2rem;
-    width: 100%;
+    width: 50%;
+
+    > h2 {
+      color: ${({ theme }) => theme.COLORS.SIMPLE_WHITE};
+      text-align: center;
+      font-family: "Poppins", sans-serif;
+      font-size: 3.2rem;
+      font-style: normal;
+      font-weight: 500;
+      line-height: 4.4rem;
+      width: 34.8rem;
+    
+      @media (max-width: 768px) {
+        display: none;
+      }
+    }
 
     > label {
       font-family: "Roboto", sans-serif;
@@ -61,16 +118,23 @@ export const FormContainer = styled.form`
 
     input {
       display: flex;
-      background-color: ${({ theme }) => theme.COLORS.BACKGROUND_HEADER};
-      min-height: 4.8rem;
-      width: 100%;
-      min-width: 31.6rem;
-      border: none;
       padding: 1.2rem 1.4rem;
-      gap: 1.4rem;
+      background-color: ${({ theme }) => theme.COLORS.BACKGROUND_INPUT};
+      border: none;
       align-items: center;
       justify-content: center;
+      gap: 1.4rem;
+      height: 4.8rem;
+      width: 31.6rem;
+      /*
+      min-height: 4.8rem;
+      min-width: 31.6rem;
       align-self: stretch;
+      */
+
+      @media (min-width: 768px) {
+        width: clamp(10rem, 40vw, 31.6rem);
+      }
     }
 
     button {
@@ -85,6 +149,20 @@ export const FormContainer = styled.form`
       font-style: normal;
       font-weight: 500;
       line-height: 2.4rem;
+    }
+
+    @media (min-width: 768px) {
+      background-color: ${({ theme }) => theme.COLORS.BACKGROUND_HEADER};
+
+      min-width: 47.6rem;
+      min-height: 62rem;
+
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      padding: 6.4rem;
+      border-radius: 1.6rem;
     }
   }
 `;
